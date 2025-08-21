9989ac99fdb706dffb862567a7c9deb8b6f8084c
bobaec <bobae.choi@gov.bc.ca>
2025-08-21 15:28:27 -0700

    refactor, add protocol

--- src/middleware/csp.ts
+++ src/middleware/csp.ts
@@ -1,11 +1,18 @@
 const cspDirectives = (req, res, next) => {
-  const baseDomain = req.get("host");
-  console.log("baseDomain", baseDomain); // keep here for logging/testing for now
-  res.setHeader(
-    "Content-Security-Policy",
-    `default-src 'self'; script-src 'self'; connect-src 'self' http://${baseDomain}`
-  );
-  next();
+  try {
+    const baseDomain = req.get("host");
+    const protocol = req.protocol;
+    console.log("baseDomain", baseDomain); // keep here for logging/testing for now
+    console.log("protocol", protocol); // keep here for logging/testing for now
+    res.setHeader(
+      "Content-Security-Policy",
+      `default-src 'self'; script-src 'self'; connect-src 'self' ${protocol}://${baseDomain}`
+    );
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 module.exports = { cspDirectives };

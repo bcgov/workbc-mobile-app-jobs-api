f6dfdb3124fb85fbd27c5bc46b1aa5778cd75f18
bobaec <bobae.choi@gov.bc.ca>
2025-08-25 11:30:16 -0700

    refactor middleware and use helmet

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+var helmet = require('helmet');
 var app = express();
 require('dotenv').config();
 var jobRouter = require('./routes/jobs.route');
-const { cspDirectives } = require('./middleware/csp.ts');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) //Parse URL-encoded bodies
@@ -12,8 +12,17 @@ app.use(cors());
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-app.use(cspDirectives);
-app.use('/jobs', jobRouter);
+app.use(helmet());
+app.use('/jobs', (req) => {
+  const baseDomain = req.get("host");
+  const protocol = req.protocol;
+  console.log('Base Domain:', `${protocol}://${baseDomain}`);
+  helmet.contentSecurityPolicy({
+    "default-src": ["'self'"],
+    "script-src": ["'self'",],
+    "content-src": ["'self'", `${protocol}://${baseDomain}`],
+  })
+}, jobRouter)
 
 app.get('/', function (req, res) {
   res.send("WORKBC-JOBS-API: Server is Running.");
